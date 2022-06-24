b0ad7cf954fbd480221e928b1cbbeb2c907ded1e
Rusty <42674949+RHQ-Rusty@users.noreply.github.com>
2022-06-23 20:07:02 -0400

    Add Serpentine Helm ChargedWeapon

--- src/main/java/com/weaponcharges/ChargedWeapon.java
+++ src/main/java/com/weaponcharges/ChargedWeapon.java
@@ -623,6 +623,54 @@ public enum ChargedWeapon
 			ChargesMessage.matcherGroupChargeMessage("Your crystal legs has ([\\d,]+) charges remaining.", 1)
 		)
 	),
+	/* Serpentine Helmet:
+    notes:
+        Number = ([\\d,]+)
+        Percent = \\(\\d+[.]?\\d%\\)
+
+    check (full, <full & >1, 1, 0/empty):
+        full: 2022-06-20 15:31:21 [Client] INFO  com.weaponcharges.Devtools - 562: GAMEMESSAGE "Scales: <col=007f00>11,000 (100.0%)</col>"
+        >1: 2022-06-20 15:32:14 [Client] INFO  com.weaponcharges.Devtools - 650: GAMEMESSAGE "Scales: <col=007f00>5 (0.1%)</col>"
+        1: 2022-06-20 15:33:02 [Client] INFO  com.weaponcharges.Devtools - 730: GAMEMESSAGE "Scales: <col=007f00>1 (0.1%)</col>"
+        empty: None
+
+    periodic updates (periodic, empty):
+        periodic: TODO
+        empty: 2022-06-20 18:19:29 [Client] INFO  com.weaponcharges.Devtools - 8380: GAMEMESSAGE "Your serpentine helm has run out of Zulrah's scales."
+        attacking when empty: TODO
+
+    adding (adding by using items on the weapon, adding via right-click option, any other methods):
+        using items: 2022-06-20 15:40:12 [Client] INFO  com.weaponcharges.Devtools - 1438: GAMEMESSAGE "Scales: <col=007f00>5 (0.1%)</col>"
+        right-click options: None
+        other: None
+
+    removing (regular removal methods, dropping):
+        regular: None
+        dropping: None
+
+    message overlap:
+        TODO
+	*/
+	SERPENTINE_HELM(new ChargedWeaponBuilder()
+		.chargedItemIds(ItemID.SERPENTINE_HELM, ItemID.TANZANITE_HELM, ItemID.MAGMA_HELM)
+		.unchargedItemIds(ItemID.SERPENTINE_HELM_UNCHARGED, ItemID.TANZANITE_HELM_UNCHARGED, ItemID.MAGMA_HELM_UNCHARGED)
+		.rechargeAmount(11_000)
+		.configKeyName("serpentine_helm")
+		.checkChargesRegexes(
+			ChargesMessage.matcherGroupChargeMessage(Text.removeTags("Scales: <col=007f00>([\\d,]+) (\\(\\d+[.]?\\d%\\))</col>"), 1)
+		)
+		.updateMessageChargesRegexes(
+			ChargesMessage.staticChargeMessage("Your serpentine helm has run out of Zulrah's scales.", 0),
+			ChargesMessage.staticChargeMessage("Your tanzanite helm has run out of Zulrah's scales.", 0),
+			ChargesMessage.staticChargeMessage("Your magma helm has run out of Zulrah's scales.", 0)
+		)
+		.dialogHandlers(
+			new ChargesDialogHandler(
+				DialogStateMatcher.inputOptionSelected(Pattern.compile("How many scales would you like to use\\? \\(0 - ([\\d,]+)\\)"), null),
+				ChargesDialogHandler.genericInputChargeMessage()
+			)
+		)
+	),
 	;
 
 	public static final List<ChargedWeapon> CRYSTAL_SHARD_RECHARGABLE_ITEMS = Arrays.asList(CRYSTAL_BOW, CRYSTAL_HELM, CRYSTAL_BODY, CRYSTAL_LEGS, BOW_OF_FAERDHINEN, CRYSTAL_HALBERD);
